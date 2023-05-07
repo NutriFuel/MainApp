4f95b77f91676fecfb97f690fd6c7c2c98136858
HarryMeng <harry.mld.2010@gmail.com>
2023-05-07 14:12:23 -0700

    Create the database schema with tables;

---
+++ client/src/components/nutrition/NutritionList.jsx
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+const axios = require('axios');
+import { useEffect, useState } from 'react';
+
+const NutritionList = (props) => {
+
+  return (
+    <div>
+      Nutrition List
+    </div>
+  )
+}
+
+export default NutritionList;
